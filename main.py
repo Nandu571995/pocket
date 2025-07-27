e08f3b9bf8f81d69bfb298a95013656ba8266614
Nandu571995 <nandkishor10000008@gmail.com>
2025-07-28 03:55:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import threading
 import subprocess
+import os
+
 from telegram_bot import start_telegram_bot
 from pocket_bot import start_pocket_bot
 
 def start_streamlit_dashboard():
-    subprocess.Popen(["streamlit", "run", "dashboard.py", "--server.port", "10000"])  # use a non-conflicting port
+    port = os.environ.get("PORT", "10000")
+    subprocess.Popen(["streamlit", "run", "dashboard.py", "--server.port", port])
 
 if __name__ == "__main__":
-    # ✅ Don't start keep_alive (conflict with streamlit)
     threading.Thread(target=start_telegram_bot).start()
     threading.Thread(target=start_pocket_bot).start()
-
-    # ✅ Launch Streamlit
     start_streamlit_dashboard()
